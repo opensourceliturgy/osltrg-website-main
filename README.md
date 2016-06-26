bdd38b433d236b3fe95aab35bb053280eebe4dc0
Sophia Shapira <Sophia.Shapira@gmail.com>
2016-06-26 01:05:06 -0400

    Added on-site rendition of missal.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # osltrg-website-main
 Web-site for the Open Source Liturgy Project
+
+For this site to work, there needs to be a specific directory
+somewhere on your local machine (but not directly-visible
+to the public) where the following packages
+are downloaded installed, all under their canonical directory-names:
+
+  * mass-missal-lng-en-rewordist
+  * mass-missal-lng-en-reclaimist
+  * mass-missal-main
+  * mass-missal-library
+  * mass-missal-lectionary
+  * osltrg-saints-primary
+
+One of the things that the arrays defined in "lcrs/main.php" will do
+is to identify the location of this directory.
+
+## Associative arrays to set up in lcrs/main.php
+
+### site_spc
+#### resdir
---
+++ missal.php
@@ -0,0 +1,34 @@
+<?php
+require "lcrs/main.php";
+
+$langpath = array (
+  array (
+    'lst' => array (
+      array (
+        'lang' => 'en'
+      )
+    ),
+    'gnr' => array()
+  )
+);
+
+
+$liturgidir = realpath($site_spc['resdir'] . "/mass-missal-main");
+$libdir = realpath($site_spc['resdir'] . "/mass-missal-library");
+$locuta = realpath($site_spc['resdir'] . "/mass-missal-lectionary");
+$canonical = realpath($site_spc['resdir'] . "/osltrg-saints-primary");
+
+
+$langpack = array(
+  "en" => array(
+    $site_spc['resdir'] . "/mass-missal-lng-en-reclaimist",
+    $site_spc['resdir'] . "/mass-missal-lng-en-rewordist"
+  )
+);
+
+$preffile = array('pref/main.php');
+$headfile = array(
+  '/path/to/resource/osltrg-style/main.php',
+);
+require($liturgidir . "/main.php");
+?>
